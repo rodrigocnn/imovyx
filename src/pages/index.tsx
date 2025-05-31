bd20cc4d965502f2b1ea23e6bf3031d659b54883
Rodrigo César <rodrigocnn@gmail.com>
2025-05-31 15:53:06 -0300

    feat(pages): update home page to be login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,73 @@
-import SiteLayout from "@/components/LayoutSite";
-import { PropertyCard } from "@/components/PropertyCard";
-
-import { Hero } from "@/components/Hero";
+import LayoutLogin from "@/components/LayoutLogin";
+import { useLogin } from "@/hooks/useLogin";
+import { useState } from "react";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, error } = useLogin();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await login(email, password);
+  };
+
   return (
-    <SiteLayout>
-      <Hero />
-
-      <div className="listings md:px-4 gap-8 lg:gap-4 justify-center  grid grid-cols-12">
-        <div className="col-span-12 md:col-span-6 lg:col-span-6 xl:col-span-4 mb-12">
-          <PropertyCard />
+    <LayoutLogin>
+      <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        Painel Administrativo
+      </h1>
+      <form
+        className="space-y-4 md:space-y-6"
+        onSubmit={handleSubmit}
+        action="#"
+      >
+        <div>
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Seu email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+            className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
         </div>
-
-        <div className="col-span-12 md:col-span-6 lg:col-span-6 xl:col-span-4 mb-12">
-          <PropertyCard />
+        <div>
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Senha
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+            className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
         </div>
-        <div className="col-span-12 md:col-span-6 lg:col-span-6 xl:col-span-4 mb-12">
-          <PropertyCard />
+        <div className="flex items-center justify-between">
+          <a
+            href="#"
+            className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+          >
+            Esqueceu a senha??
+          </a>
         </div>
-
-        <div className="col-span-12 md:col-span-6 lg:col-span-6 xl:col-span-4 mb-12">
-          <PropertyCard />
-        </div>
-
-        <div className="col-span-12 md:col-span-6 lg:col-span-6 xl:col-span-4 mb-12">
-          <PropertyCard />
-        </div>
-        <div className="col-span-12 md:col-span-6 lg:col-span-6 xl:col-span-4 mb-12">
-          <PropertyCard />
-        </div>
-
-        <div className="col-span-12 md:col-span-6 lg:col-span-6 xl:col-span-4 mb-12">
-          <PropertyCard />
-        </div>
-
-        <div className="col-span-12 md:col-span-6 lg:col-span-6 xl:col-span-4 mb-12">
-          <PropertyCard />
-        </div>
-
-        <div className="col-span-12 md:col-span-6 lg:col-span-6 xl:col-span-4 mb-12">
-          <PropertyCard />
-        </div>
-      </div>
-    </SiteLayout>
+        <button
+          type="submit"
+          className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+        >
+          Entrar
+        </button>
+        {error && <p className="bg-red-500 text-white p-2 rounded">{error}</p>}
+      </form>
+    </LayoutLogin>
   );
 }
