f778f96a9829d405e9a82437d748cd5ef3eb5e29
Rodrigo César <rodrigocnn@gmail.com>
2025-06-03 13:48:00 -0300

    feat:(login) add diabled when isLoading yet

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -65,6 +65,7 @@ export default function Login() {
         </div>
         <button
           type="submit"
+          disabled={isLoading}
           className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
         >
           {isLoading ? "Carregando servidor" : "Entrar"}
